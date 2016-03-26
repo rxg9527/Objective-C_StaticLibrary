f40841e7b21494561a00fee6f72b996e6112a7ec
Yuen-rMBP <rxg9527@sina.cn>
2016-03-26 12:55:57 +0800

    Initial Commit

---
+++ Objective-C_XGStaticFramework/Objective_C_XGStaticFramework.h
@@ -0,0 +1,13 @@
+//
+//  Objective_C_XGStaticFramework.h
+//  Objective-C_XGStaticFramework
+//
+//  Created by Yuen on 16/3/26.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Objective_C_XGStaticFramework : NSObject
+
+@end
---
+++ Objective-C_XGStaticFramework/Objective_C_XGStaticFramework.m
@@ -0,0 +1,13 @@
+//
+//  Objective_C_XGStaticFramework.m
+//  Objective-C_XGStaticFramework
+//
+//  Created by Yuen on 16/3/26.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "Objective_C_XGStaticFramework.h"
+
+@implementation Objective_C_XGStaticFramework
+
+@end
