b71a375c899ab003fb07e13fd4442bfed71a39a5
Yuen-rMBP <rxg9527@sina.cn>
2016-03-27 23:22:56 +0800

    0327 新建类，并写上一个示例方法

--- Objective-C_XGStaticFramework/XGTool.h
+++ Objective-C_XGStaticFramework/XGTool.h
@@ -1,13 +1,15 @@
 //
-//  Objective_C_XGStaticFramework.m
+//  XGTool.h
 //  Objective-C_XGStaticFramework
 //
 //  Created by Yuen on 16/3/26.
 //  Copyright © 2016年 Yuen. All rights reserved.
 //
 
-#import "Objective_C_XGStaticFramework.h"
+#import <Foundation/Foundation.h>
 
-@implementation Objective_C_XGStaticFramework
+@interface XGTool : NSObject
+
++ (void)logFunctionName;
 
 @end
--- Objective-C_XGStaticFramework/XGTool.m
+++ Objective-C_XGStaticFramework/XGTool.m
@@ -1,13 +1,17 @@
 //
-//  Objective_C_XGStaticFramework.h
+//  XGTool.m
 //  Objective-C_XGStaticFramework
 //
 //  Created by Yuen on 16/3/26.
 //  Copyright © 2016年 Yuen. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "XGTool.h"
 
-@interface Objective_C_XGStaticFramework : NSObject
+@implementation XGTool
+
++ (void)logFunctionName {
+    NSLog(@"%s", __FUNCTION__);
+}
 
 @end
