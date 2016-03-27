4d3080af9a58d77bd39e5f9c8f767b6655780005
Yuen-rMBP <rxg9527@sina.cn>
2016-03-27 23:40:37 +0800

    0327 修改BuildSettings的ONLY_ACTIVE_ARCH为NO，这样生成的.a文件就不会只支持当前选中的编译设备的架构 不同设备架构  模拟器  4s-->5 : i386  5s-->6plus : x86_64
     真机
     3gs-->4s : armv7
     5/5c : armv7s,静态库只要支持了armv7,就可以跑在armv7s的架构上
     5s-->6plus : arm64

--- Objective-C_XGStaticFramework/XGTool.m
+++ Objective-C_XGStaticFramework/XGTool.m
@@ -10,6 +10,23 @@
 
 @implementation XGTool
 
+/*
+ 每一个设备都有属于自己的CPU架构(4s/6plus)
+ 每一个静态支持的架构是固定(liblibstatic.a)
+ 查看静态库支持的架构:lipo -info liblibstatic.a
+ 
+ 静态库合并:
+ lipo -create 静态库1 静态库2 -output 新的静态库
+ 
+ 模拟器
+ 4s-->5 : i386
+ 5s-->6plus : x86_64
+ 
+ 真机
+ 3gs-->4s : armv7
+ 5/5c : armv7s,静态库只要支持了armv7,就可以跑在armv7s的架构上
+ 5s-->6plus : arm64
+ */
 + (void)logFunctionName {
     NSLog(@"%s", __FUNCTION__);
     NSLog(@"%s", __FUNCTION__);
